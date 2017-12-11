e8f0e2d6bcafb14b9a9bb5576228853a7550c8a7
Gaurav Kumar <gaurav.kumar9825@gmail.com>
2017-12-11 22:56:35 +0530

    remove files

--- to-Do/app/models/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-const Sequelize = require('sequelize')
-
-require('dotenv').config()
-
-const env = process.env.NODE_ENV || 'development'
-
-const config = require(path.join(__dirname, '../../', 'config', 'config.js'))[env]
